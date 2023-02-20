036d8117e3467698667a7852fd78dd95d54f7cb1
Fábio Penha <fabiopenha95@outlook.com>
2023-02-20 11:54:18 -0300

    first commit

---
+++ ConversorDeMoeda/src/conversor/Conversor.java
@@ -0,0 +1,43 @@
+package conversor;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class Conversor {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Conversor window = new Conversor();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public Conversor() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
